cd89271139f9b11955bb776225b9c07fa914cfc7
andrewjjj <juwon1@ualberta.ca>
2021-09-17 01:45:36 -0600

    Done lab 1

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -2,4 +2,5 @@ import requests
 print(f"requests version: {requests.__version__}")
 print()
 req_google = requests.get("https://www.google.com")
-print(req_google.content)
+req_myfile = requests.get("https://raw.githubusercontent.com/Andrewjjj/CMPUT404Lab/main/Lab1/lab1.py")
+print(req_myfile.content)
